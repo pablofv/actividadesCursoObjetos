401e7c513d8f53db1b7d7dfc80f6de50138e7699
pablofv <tramitesyregistraciones@gmail.com>
2022-04-24 01:21:05 -0300

    Update Testactividad2.java
    El último punto, vehículos ordenados por orden natural.

--- segundaActividad/src/main/java/ar/org/centro8/activadad2/java/actividad2/test/Testactividad2.java
+++ segundaActividad/src/main/java/ar/org/centro8/activadad2/java/actividad2/test/Testactividad2.java
@@ -69,5 +69,18 @@ public class Testactividad2 {
                     .stream()
                     .sorted(Comparator.comparing(v->v.getPrecio()*-1))
                     .forEach(v->System.out.println(v.getMarca() + " " + v.getModelo()));
+        
+        System.out.println("\n\n=============================\n\n");
+
+        //Ordenados por compareTO (orden natural)
+        // Vehículos ordenados por orden natural (por marca,modelo,precio):
+        // Marca: Honda // Modelo: Titan // Cilindrada: 125c // Precio: $60.000,00
+        // Marca: Peugeot // Modelo: 206 // Puertas: 4 // Precio: $200.000,00
+        // Marca: Peugeot // Modelo: 208 // Puertas: 5 // Precio: $250.000,00
+        // Marca: Yamaha // Modelo: YBR // Cilindrada: 160c // Precio: $80.500,50
+        colVehiculos
+                    .stream()
+                    .sorted()
+                    .forEach(System.out::println);
     }
 }
