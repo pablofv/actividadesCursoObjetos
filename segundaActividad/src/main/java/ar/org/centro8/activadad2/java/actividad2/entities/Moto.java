889693affb2777d725ffbf0f4e1d378163248b89
pablofv <tramitesyregistraciones@gmail.com>
2022-04-23 23:48:38 -0300

    Update Moto.java
    Hacemos lo mismo con las motos, del padre tomamos los primeros dos datos, de marca y modelo, agregamos la cilindrada y luego pedimos el precio y lo agregamos, formateado.

--- segundaActividad/src/main/java/ar/org/centro8/activadad2/java/actividad2/entities/Moto.java
+++ segundaActividad/src/main/java/ar/org/centro8/activadad2/java/actividad2/entities/Moto.java
@@ -1,5 +1,7 @@
 package ar.org.centro8.activadad2.java.actividad2.entities;
 
+import java.text.DecimalFormat;
+
 public class Moto extends Vehiculo{
     private String cilindrada;
 
@@ -10,7 +12,10 @@ public class Moto extends Vehiculo{
 
     @Override
     public String toString() {
-        return "Moto " + super.toString() + " // Cilindrada:" + cilindrada + "]";
+        DecimalFormat df=new DecimalFormat("###,###.00");
+        //Modelo de ejmplo de salida
+        //Marca: Yamaha // Modelo: YBR // Cilindrada: 160c // Precio: $80.500,50
+        return super.toString() + " // Cilindrada: " + cilindrada + " // Precio: $" + df.format(this.getPrecio());
     }
     
 }
