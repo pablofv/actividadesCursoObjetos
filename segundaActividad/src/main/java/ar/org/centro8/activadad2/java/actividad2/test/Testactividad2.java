43e1a8dc20b3194a7bdd3e3babb9e7c1afb11f7f
pablofv <tramitesyregistraciones@gmail.com>
2022-04-24 01:24:23 -0300

    Update Testactividad2.java
    Quito comentarios que ya no quiero

--- segundaActividad/src/main/java/ar/org/centro8/activadad2/java/actividad2/test/Testactividad2.java
+++ segundaActividad/src/main/java/ar/org/centro8/activadad2/java/actividad2/test/Testactividad2.java
@@ -73,11 +73,6 @@ public class Testactividad2 {
         System.out.println("\n\n=============================\n\n");
 
         //Ordenados por compareTO (orden natural)
-        // Vehículos ordenados por orden natural (por marca,modelo,precio):
-        // Marca: Honda // Modelo: Titan // Cilindrada: 125c // Precio: $60.000,00
-        // Marca: Peugeot // Modelo: 206 // Puertas: 4 // Precio: $200.000,00
-        // Marca: Peugeot // Modelo: 208 // Puertas: 5 // Precio: $250.000,00
-        // Marca: Yamaha // Modelo: YBR // Cilindrada: 160c // Precio: $80.500,50
         colVehiculos
                     .stream()
                     .sorted()
