94d48cf2c93f3bad962845ec8044d2a77e598c2d
pablofv <tramitesyregistraciones@gmail.com>
2022-06-01 21:47:58 -0300

    Add files via upload
    Me había faltado guardar el archivo script de la base

--- Proyecto Integrador/proyecto/src/main/java/ar/org/centro8/curso/java/proyecto/sql/modelo reducido.sql
+++ Proyecto Integrador/proyecto/src/main/java/ar/org/centro8/curso/java/proyecto/sql/modelo reducido.sql
@@ -2,68 +2,50 @@ DROP DATABASE if EXISTS distribuidora;
 CREATE DATABASE distribuidora;
 use distribuidora;
 
-DROP TABLE if EXISTS tipoProducto;
 DROP TABLE if EXISTS productos;
-DROP TABLE if EXISTS direccion;
 DROP TABLE if EXISTS colegios;
-DROP TABLE if EXISTS remitos;
+-- DROP TABLE if EXISTS remitos;
 DROP TABLE if EXISTS facturas;
 DROP TABLE if EXISTS item_facturas;
-
-create table tipoProducto(
-	id int auto_increment primary key,
-    tipo varchar(100) not null,
-    sub_tipo varchar(100) not null
-);
 
 create table productos(
 	id int auto_increment primary key,
     nombre varchar(100) not null,
     marca varchar(40) not null,
-    id_tipo_producto int not null,
+    tipo_producto varchar(100) not null,
     precio_costo int not null,
     unidad varchar(100) not null,
     cantidad_por_unidad int not null
 );
 
-alter table productos
-add constraint foreign key (id_tipo_producto) references tipoProducto(id);
-
-create table direccion(
-	id int auto_increment primary key,
-	calle varchar(100),
-    numero int,
-    localidad varchar(100),
-    codigoPostal int
-);
 create table colegios(
     id int auto_increment primary key,
 	nombre varchar(100) not null,
-    id_direccion int not null
+    direccion varchar(100) not null
 );
-
-alter table colegios
-add constraint foreign key (id_direccion) references direccion(id);
-   
+/*   
 create table remitos(
 	id int auto_increment primary key,
-    destinatario int, /* colegio donde se entrega */
+    id_colegio int, -- colegio donde se entrega
     id_producto int,
     cantidad_producto int,
     fecha_remito timestamp
 );
 alter table remitos
-add constraint foreign key(destinatario) references colegio(id);
+add constraint foreign key(id_colegio) references colegios(id);
 alter table remitos
 add constraint foreign key(id_producto) references productos(id);
-
+*/
 
 create table facturas(
 	id_factura int auto_increment primary key,
     fecha_factura timestamp,
     total_facturado double,
-    facturadoA int /*a quien se le factura*/
+    id_colegio int /*a quien se le factura*/
 );
+
+alter table facturas
+add constraint foreign key cf_facturas_colegio (id_colegio) references colegios(id);
 
 create table itemFacturas(
 	id_item int auto_increment primary key,
@@ -74,6 +56,6 @@ create table itemFacturas(
 );
 
 alter table itemFacturas
-add constraint foreign key(id_factura) references facturas(id_factura);
-alter table itemFacturas
-add constraint foreign key(id_producto) references productos(id);
+add constraint foreign key cf_items_facturas (id_factura) references facturas(id_factura),
+-- alter table itemFacturas
+add constraint foreign key cf_items_productos (id_producto) references productos(id);
