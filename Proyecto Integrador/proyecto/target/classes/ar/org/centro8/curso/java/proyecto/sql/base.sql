59376fdd8e752131cf9a63df719cbdf5aa7b4ccc
pablofv <tramitesyregistraciones@gmail.com>
2022-06-03 20:03:41 -0300

    Cambios al modelo
    Quité las tablas que no van mas, cambié nombres de campos que había cambiado, cambié el orden de las tablas para que las cree y borre sin problemas.

--- Proyecto Integrador/proyecto/target/classes/ar/org/centro8/curso/java/proyecto/sql/base.sql
+++ Proyecto Integrador/proyecto/target/classes/ar/org/centro8/curso/java/proyecto/sql/base.sql
@@ -1,79 +1,45 @@
+/*
 DROP DATABASE if EXISTS distribuidora;
 CREATE DATABASE distribuidora;
 use distribuidora;
-
-DROP TABLE if EXISTS tipoProducto;
-DROP TABLE if EXISTS productos;
-DROP TABLE if EXISTS direccion;
-DROP TABLE if EXISTS colegios;
-DROP TABLE if EXISTS remitos;
+*/
+DROP TABLE if EXISTS itemFacturas;
 DROP TABLE if EXISTS facturas;
-DROP TABLE if EXISTS item_facturas;
-
-create table tipoProducto(
-	id int auto_increment primary key,
-    tipo varchar(100) not null,
-    sub_tipo varchar(100) not null
-);
+DROP TABLE if EXISTS colegios;
+DROP TABLE if EXISTS productos;
 
 create table productos(
 	id int auto_increment primary key,
     nombre varchar(100) not null,
     marca varchar(40) not null,
-    id_tipo_producto int not null,
-    precio_costo int not null,
-    unidad varchar(100) not null,
-    cantidad_por_unidad int not null
+    tipoProducto enum("Carnes", "Verduras", "Panificados", "Frutas", "Lacteos", "Bebidas"),
+    precioCosto int not null,
+    unidad enum("Kg", "Lt", "Unidad"),
+    cantidadPorUnidad int not null
 );
 
-alter table productos
-add constraint foreign key (id_tipo_producto) references tipoProducto(id);
-
-create table direccion(
-	id int auto_increment primary key,
-	calle varchar(100),
-    numero int,
-    localidad varchar(100),
-    codigoPostal int
-);
 create table colegios(
     id int auto_increment primary key,
 	nombre varchar(100) not null,
-    id_direccion int not null
+    direccion varchar(100) not null
 );
-
-alter table colegios
-add constraint foreign key (id_direccion) references direccion(id);
-   
-create table remitos(
-	id int auto_increment primary key,
-    destinatario int, /* colegio donde se entrega */
-    id_producto int,
-    cantidad_producto int,
-    fecha_remito timestamp
-);
-alter table remitos
-add constraint foreign key(destinatario) references colegio(id);
-alter table remitos
-add constraint foreign key(id_producto) references productos(id);
-
 
 create table facturas(
-	id_factura int auto_increment primary key,
-    fecha_factura timestamp,
-    total_facturado double,
-    facturadoA int /*a quien se le factura*/
+	id int auto_increment primary key,
+    fechaFactura timestamp,
+    totalFacturado double,
+    idColegio int /*a quien se le factura*/
 );
 
 create table itemFacturas(
-	id_item int auto_increment primary key,
-    id_factura int not null,
-    id_producto int not null,
-    cantidad_producto double not null,
-    precio_unitario double not null
+	id int auto_increment primary key,
+    idFactura int not null,
+    idProducto int not null,
+    cantidadProducto double not null,
+    precioUnitario double not null
 );
 
 alter table itemFacturas
-add constraint foreign key(id_factura) references facturas(id_factura);
+add constraint foreign key(idFactura) references facturas(id);
 alter table itemFacturas
-add constraint foreign key(id_producto) references productos(id);
+add constraint foreign key(idProducto) references productos(id);
