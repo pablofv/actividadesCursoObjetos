1c4adbb51022532760f373ad74e409ed3732403a
pablofv <tramitesyregistraciones@gmail.com>
2022-07-13 16:41:33 -0300

    Create I_TablaMostrarFacturasRepository.java
    Interfaz del repositorio que quiero utilizar para mostrar una tabla.

---
+++ Proyecto Integrador/proyecto/src/main/java/ar/org/centro8/curso/java/proyecto/repositorios/interfaces/I_TablaMostrarFacturasRepository.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ar.org.centro8.curso.java.proyecto.repositorios.interfaces;
+
+import java.util.List;
+
+import ar.org.centro8.curso.java.proyecto.entities.TablaMostrarFacturas;
+
+/**
+ *
+ * @author Legion
+ */
+public interface I_TablaMostrarFacturasRepository {
+    List<TablaMostrarFacturas>getAll();
+}
